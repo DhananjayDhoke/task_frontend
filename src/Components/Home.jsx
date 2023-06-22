8747df24c76730e62ce20b38baabd7681d5d0af5
DhananjayDhoke <dhananjaydhoke3@gmail.com>
2023-06-22 15:50:29 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,22 @@
+
+import "./Home.css"
+const Home = () => {
+  return (
+    <div>
+       <div className="home-main-div">
+           <div className="home-header-div">
+                 <div className="home-header-logo-div">
+                    <img src="/images/Pro-Legal-Serve-Logo 1.png" alt="" />
+                </div>  
+                 <div className="home-header-profile-div"></div>
+           </div>
+           <div className="home-footer-div">
+              <div className="home-left-div"></div>
+              <div className="home-right-div"></div>
+           </div>
+       </div>
+    </div>
+  )
+}
+
+export default Home
